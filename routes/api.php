ccdec6602a524c2bc9fae4429b15d07ab658c711
Your Name <you@example.com>
2020-09-13 12:33:45 -0300

    Modificaçoes nas rotas da agenda

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,10 @@ $app->add(new \Agenda\Controller\AutenticacaoApiMiddleware());
 
 $app->group('/api/v1/agenda', function () use($app){
     $app->get('/', '\Agenda\Controller\AgendaController:readAll');
+    $app->get('/:id', '\Agenda\Controller\AgendaController:readById');
     $app->post('/', '\Agenda\Controller\AgendaController:create');
-    $app->put('/', '\Agenda\Controller\AgendaController:home');
-    $app->delete('/', '\Agenda\Controller\AgendaController:home');
+    $app->put('/', '\Agenda\Controller\AgendaController:update');
+    $app->delete('/:id', '\Agenda\Controller\AgendaController:delete');
 });
 
 $app->group('/api/v1/api', function () use($app){
