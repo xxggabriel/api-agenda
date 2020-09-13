0f1d6d03b1ef67dd93fbdbe5842affc7e6a86f01
Your Name <you@example.com>
2020-09-13 13:34:25 -0300

    Funçoes para manipulaçao da API_KEY criadas

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -15,22 +15,32 @@ class ApiController extends Controller{
         
     }
 
-    public function create($request, $response, $args)
+    public function create()
     {
-        if(!$this->validateUser($args["user"])){
-            return $this->return(true, "Usuário não existe!");
-        }
+        $user = $this->getParam("user_id");
+        $status = $this->getParam("status") == "" ?  1 : $this->getParam("status");
         
-        $user = $args["user"];
-        $key =  md5(uniqid($user, true));
-
-        if(!$this->api->create($key, $user)){
-            return $this->return(true, "Erro ao salvar a key, tente novamente");
+        if(empty($user)){
+            return $this->return(true, "Erro, é obrigátorio ser passado todos os parâmetro (user, [status])");
         }
 
-        $this->return(false, "", [
-            "api_key" => $key
-        ]);
+        if(!$this->api->readUser($user)){
+            return $this->return(true, "Erro, user não existe");
+        }
+
+        $api_key =  md5(uniqid($user, true));
+
+        $result = $this->api->create($user,$api_key, $status);
+
+        if($result){
+            return $this->return(false, "Api gerada com sucesso", [
+                "user" => $user,
+                "api_key" => $api_key,
+                "status" => $status,
+            ]);
+        } else {
+            return $this->return(true, "Erro ao criar a api_key, tente novamente");
+        }    
     }
 
     public function validateUser($user)
@@ -38,10 +48,54 @@ class ApiController extends Controller{
         return empty($this->api->readUser($user)) ? false : true;
     }
 
-
-    public function delete($key)
+    public function readAll()
     {
+        return $this->return(false, "", $this->api->readAll());
+    }
+
+    public function readById($id)
+    {
+        return $this->return(false, "", $this->api->readById($id));
+    }
+
+    public function update($id)
+    {
+        $user = $this->getParam("user_id");
+        $status = $this->getParam("status") == "" ?  1 : $this->getParam("status");
         
+
+        if(empty($user)){
+            return $this->return(true, "Erro, é obrigátorio ser passado todos os parâmetro (user, [status])");
+        }
+
+        if(!$this->api->readUser($user)){
+            return $this->return(true, "Erro, user não existe");
+        }
+
+
+        $result = $this->api->update($id, $user, $status);
+
+        if($result){
+            return $this->return(false, "Api atualizada com sucesso");
+        } else {
+            return $this->return(true, "Erro ao atualizar a api_key, tente novamente");
+        }
+    }
+
+
+    public function delete($id)
+    {
+
+        if(!$this->api->readById($id)){
+            return $this->return(true, "Erro, id não existe");
+        }
+        $result = $this->api->delete($id);
+
+        if($result){
+            return $this->return(false, "Api apagada com sucesso");
+        } else {
+            return $this->return(true, "Erro ao apagar a api_key, tente novamente");
+        }
     }
 
 
--- src/Model/Api.php
+++ src/Model/Api.php
@@ -7,32 +7,54 @@ use Agenda\Model\DB\Sql;
 class Api extends Sql{
 
 
-    public function create($key, $user)
+    public function create($user, $api_key, $status = 1)
     {
-        return $this->query("INSERT INTO api (api_key, user) VALUES (:api_key, :user)", [
-            ":api_key"  => $key,
-            ":user" => $user
+        return $this->query("INSERT INTO api (api_key, user, status) VALUES (:api_key, :user, :status)", [
+            ":api_key"  => $api_key,
+            ":user" => $user,
+            ":status" => $status
         ]);
     }
 
     public function readUser($user)
     {
         return $this->select("SELECT * FROM api WHERE user = :user", [
-            ":user"  => $user
+            ":user" => $user
         ]);
     }
 
-    public function readKey($key)
+    public function readKey($api_key)
     {
         return $this->select("SELECT * FROM api WHERE api_key = :api_key", [
-            ":api_key"  => $key
+            ":api_key" => $api_key
         ])[0];
     }
 
-    public function delete($key)
+    public function readAll()
     {
-        return $this->query("DELETE FROM api WHERE api_key = :api_key", [
-            ":api_key"  => $key
+        return $this->select("SELECT * FROM api");
+    }
+
+    public function readById($id)
+    {
+        return $this->select("SELECT * FROM api WHERE id = :id", [
+            ":id"  => $id
+        ]);
+    }
+
+    public function update($id, $user, $status = 1)
+    {
+        return $this->query("UPDATE api SET user = :user, status = :status WHERE id = :id", [
+            ":id"           => $id,
+            ":user"       => $user,
+            ":status"    => $status,
+        ]);
+    }
+
+    public function delete($id)
+    {
+        return $this->query("DELETE FROM api WHERE id = :id", [
+            ":id"  => $id
         ]);
     }
 
