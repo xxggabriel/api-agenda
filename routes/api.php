4071c7577ad5fd0c61c8aa14343f948190b16c6b
Your Name <you@example.com>
2020-09-13 12:16:40 -0300

    Rotas da agenda criada

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,32 @@
 <?php 
+header('Content-Type: application/json');
+use \Slim\Slim;
 
-$app = new \Slim\App();
+$config = [
+    'settings' => [
+        'displayErrorDetails' => true,
 
-$app->group('/api/v1/', function (App $app) {
-    $app->get('/', '\Agenda\Controller\AgendaController:home');
+    ],
+];
+$app = new Slim($config);
+
+$app->add(new \Agenda\Controller\AutenticacaoApiMiddleware());
+
+$app->group('/api/v1/agenda', function () use($app){
+    $app->get('/', '\Agenda\Controller\AgendaController:readAll');
+    $app->post('/', '\Agenda\Controller\AgendaController:create');
+    $app->put('/', '\Agenda\Controller\AgendaController:home');
+    $app->delete('/', '\Agenda\Controller\AgendaController:home');
+});
+
+$app->group('/api/v1/api', function () use($app){
+    $app->get('/', '\Agenda\Controller\AgendaController:getAgenda');
     $app->post('/', '\Agenda\Controller\AgendaController:home');
     $app->put('/', '\Agenda\Controller\AgendaController:home');
     $app->delete('/', '\Agenda\Controller\AgendaController:home');
-})->add( new \Agenda\Controller\AutenticacaoApiMiddleware() );
+});
+
+
+$app->run();
+
+
