7b6f54a012c83115d08460d9fbbdfa3d6b524ba1
Your Name <you@example.com>
2020-09-13 13:31:42 -0300

    Rotas de manipulaçao da API_KEY criadas

--- routes/api.php
+++ routes/api.php
@@ -14,17 +14,18 @@ $app->add(new \Agenda\Controller\AutenticacaoApiMiddleware());
 
 $app->group('/api/v1/agenda', function () use($app){
     $app->get('/', '\Agenda\Controller\AgendaController:readAll');
-    $app->get('/:id', '\Agenda\Controller\AgendaController:readById');
     $app->post('/', '\Agenda\Controller\AgendaController:create');
-    $app->put('/', '\Agenda\Controller\AgendaController:update');
+    $app->get('/:id', '\Agenda\Controller\AgendaController:readById');
+    $app->put('/:id', '\Agenda\Controller\AgendaController:update');
     $app->delete('/:id', '\Agenda\Controller\AgendaController:delete');
 });
 
 $app->group('/api/v1/api', function () use($app){
-    $app->get('/', '\Agenda\Controller\AgendaController:getAgenda');
-    $app->post('/', '\Agenda\Controller\AgendaController:home');
-    $app->put('/', '\Agenda\Controller\AgendaController:home');
-    $app->delete('/', '\Agenda\Controller\AgendaController:home');
+    $app->get('/', '\Agenda\Controller\ApiController:readAll');
+    $app->post('/', '\Agenda\Controller\ApiController:create');
+    $app->get('/:id', '\Agenda\Controller\ApiController:readById');
+    $app->put('/:id', '\Agenda\Controller\ApiController:update');
+    $app->delete('/:id', '\Agenda\Controller\ApiController:delete');
 });
 
 
